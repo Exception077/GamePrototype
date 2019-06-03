47bca24152da71e06301ca2aeb94efcb5d6aba11
GuidanceX <48905421+Exception077@users.noreply.github.com>
2019-06-03 15:31:49 +0800

    Inspector扩展

---
+++ Script/Editor/InitNPCEditor.cs
@@ -0,0 +1,26 @@
+﻿/*********************************************************************************
+ *Copyright(C) 2015 by Gx
+ *All rights reserved.
+ *FileName:     InitNPCEditor.cs
+ *Author:       Gx
+ *Version:      1.0
+ *UnityVersion：2018.3.10f1
+ *Date:         2019-06-02
+ *Description:   
+ *History:  
+**********************************************************************************/
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(InitNPC))]
+public class InitNPCEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        InitNPC myScript = (InitNPC)target;
+        if (GUILayout.Button("ADD ITEM")) {
+            myScript.addItem();
+        }
+    }
+}
